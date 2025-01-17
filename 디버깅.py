ded3636ed568fafd7f0f40c74f37fa4975413159
yuchanKIM <kyc4537@naver.com>
2025-01-17 15:53:44 +0900

    debuging

---
+++ 디버깅.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    print('디버깅')
+    print('디버깅')
+    print('디버깅')
+    return a+b
+
+
+add(5,6)
+
+print('디버깅')
+print('디버깅')
+print('디버깅') # 중단점
+print('디버깅')
+print('디버깅')
+print('디버깅')
